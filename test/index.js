9b117cd4d86ec84b1464b0fc8fe0602b4c77a49e
andor <latandor@gmail.com>
2022-07-06 09:50:14 -0400

    setting up server

--- test/index.js
+++ test/index.js
@@ -4,13 +4,52 @@ const type = document.getElementById(`new-task-description`)
 const list = document.getElementById(`list-items`)
 const list2 = document.getElementById(`previous-days`)
 const btn = document.getElementById(`create-task-form`)
+const btn3 = document.getElementById(`add-btn`)
+const infoUrl = `https://anapioficeandfire.com/api`
+
+
+
+
+
+
+const fetchData = (id) => {
+ fetch(`https://anapioficeandfire.com/api/books/${id}`)
+ .then(res => res.json())
+ .then(req => {
+  renderHeader(req)
+ }).catch(err => {
+  console.log(`Here is the ${err}`)
+ })
+}
+
+
+
+/*
+const changeTitle = () => {
+for(let i = 0; i < 10; i++)
+
+fetchData(i)
+}
+
+
+btn3.addEventListener(`click`, (e) => {
+ changeTitle(e)
+})
+
+*/
+
+
+
+
 
 btn.addEventListener(`submit`, (e) => {
  e.preventDefault()
- const li = document.createElement(`li`)
- li.innerText = type.value
- list.append(li) 
- type.innerText = ``
+ let addReminder = {
+  title: e.target.Title.value,
+  note: e.target.Note.value,
+ } 
+ 
+
 
  const deleteBtn = document.createElement(`button`)
  deleteBtn.innerText = `Delete`
@@ -20,27 +59,43 @@ btn.addEventListener(`submit`, (e) => {
   li.remove()
  
  })
+ 
 })
 
-
-const renderData = () => {
-
- console.log(test.something.length)
+const renderBookCard = () =>{
+ const li = document.createElement(`li`)
+ 
 }
 
 
 
-
-const renderFooter = (store) =>{
+const renderFooter = (book) =>{
  let footerDiv = document.querySelectorAll(`footer div`)
- footerDiv[1].innerText = store.name
+ footerDiv[0].innerText = book.name
+ footerDiv[1].innerText = book.name
+ footerDiv[2].innerText = book.name
 }
 
-renderFooter(1)
 
-const renderHeader = (store) => {
+const renderHeader = (book) => {
 const header = document.getElementById(`header-title`)
-header.innerText = store.name
+header.innerText = book.name
 }
 
-renderHeader(1)
+const createResource = (url, body) => {
+ return fetch(url, {
+  method: `POST`,
+  headers: {
+   `content-type`: `application/json`
+  },
+  body: json (body)
+ })
+ .then(res =>res.json())
+}
+
+
+const renderBook = (e) => {
+ const book ={
+  title: e.target.name.value
+ }
+}
