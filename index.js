019d1475386bb53e977dfe4c7fe1d630d7f977ce
andor <latandor@gmail.com>
2022-07-05 21:16:35 -0400

    fetch request preparation

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 
 
-const type = document.getElementById(`type-list`)
-const list = document.getElementById(`list`)
+const type = document.getElementById(`new-task-description`)
+const list = document.getElementById(`list-items`)
 const list2 = document.getElementById(`previous-days`)
+const btn = document.getElementById(`create-task-form`)
 
-
-const btn = document.getElementById(`add-btn`).addEventListener(`click`, () => {
+btn.addEventListener(`submit`, (e) => {
+ e.preventDefault()
  const li = document.createElement(`li`)
  li.innerText = type.value
  list.append(li) 
  type.innerText = ``
- 
-
 
  const deleteBtn = document.createElement(`button`)
  deleteBtn.innerText = `Delete`
@@ -24,6 +23,24 @@ const btn = document.getElementById(`add-btn`).addEventListener(`click`, () => {
 })
 
 
-const renderData = (data) => {
+const renderData = () => {
+
  console.log(test.something.length)
 }
+
+
+
+
+const renderFooter = (store) =>{
+ let footerDiv = document.querySelectorAll(`footer div`)
+ footerDiv[1].innerText = store.name
+}
+
+renderFooter(1)
+
+const renderHeader = (store) => {
+const header = document.getElementById(`header-title`)
+header.innerText = store.name
+}
+
+renderHeader(1)
---
+++ test/data.js
@@ -0,0 +1,16 @@
+const reminder = {
+   name: `This is a Reminder Applicaiton`,
+   date: `Created on: 07/05/2022`,
+   note: `FLatiron School`,
+   reminderOf: [
+    {
+     id: 1
+    },
+    {
+     id: 2
+    },
+    {
+     id: 3
+    }
+   ]
+}
